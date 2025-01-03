caf4b3cfb6b089708ba73ccd91e68ca981a0fc16
Michael Conan <michaelbconan@gmail.com>
2025-01-03 11:56:21 -0800

    feat: add honcho Procfile, airflow config, startup command

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run metadata database migrations
-airflow db migrate
-
-# Run scheduler and webserver on the same container
-airflow scheduler & airflow webserver --port ${PORT:-8080}
--- webserver_config.py
+++
@@ -1,7 +0,0 @@
-"""
-webserver_config.py
-
-Configuration of Airflow webserver and definition of Flask app object,
-which may be beneficial for Azure app service custom startup.
-"""
-from flask import current_app as app
